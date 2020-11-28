1bfebed79f075226ac147ffb143b8992449483cc
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-11-28 13:05:04 -0500

    more output

--- _scripts/combine.py
+++ _scripts/combine.py
@@ -9,13 +9,17 @@ def combine():
 
     all_files = sorted(P.ATEST_OUT.rglob(P.ATEST_OUT_XML))
     all_output_dirs = sorted({p.parent for p in all_files})
-    [print(f"- {p.relative_to(P.ATEST_OUT)}") for p in all_output_dirs]
+    print(f"all output folders in {P.ATEST_OUT}", flush=True)
+    [print(f"- {p.relative_to(P.ATEST_OUT)}", flush=True) for p in all_output_dirs]
 
     all_robot = [
         p
         for p in P.ATEST_OUT.rglob(P.ATEST_OUT_XML)
         if p != final and "pabot_results" not in str(p)
     ]
+    print(f"all {P.ATEST_OUT_XML} in {P.ATEST_OUT}", flush=True)
+    [print(f"- {p.relative_to(P.ATEST_OUT)}", flush=True) for p in all_robot]
+
     args = [
         "python",
         "-m",
