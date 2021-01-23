9257146a47d37a6bfce10bd669861d193cfed185
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-01-23 15:22:12 -0500

    update some locks with lab2rc

--- dodo.py
+++ dodo.py
@@ -10,10 +10,7 @@ from _scripts import project as P
 from _scripts.reporter import GithubActionsReporter
 
 
-os.environ.update(
-    PYTHONIOENCODING="utf-8",
-    MAMBA_NO_BANNER="1"
-)
+os.environ.update(PYTHONIOENCODING="utf-8", MAMBA_NO_BANNER="1")
 
 DOIT_CONFIG = {
     "backend": "sqlite3",
